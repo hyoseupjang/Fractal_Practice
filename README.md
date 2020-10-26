1418d83fd311740881367b988019edab5a471f86
Hyoseup Jang <hyoseupjang@gmail.com>
2020-10-27 00:31:21 +0900

    Improvement: Use Fortran binary format to speed up. Some sample added.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Fractal_Practice
 Just for(tran) practice. 
 
+![Sample_Mandelbrot](./Sample/Mandelbrot.png)
+
 ## How to compile. 
-You can use your favorite compiler. 
+You can use your favorite compiler. Before you compile, change thread number 20 to appropriate number for your system. This code dosen't support parallelization for multi-node system. 
 
 ### With GCC Fortran
 gfortran fractal.f90 -o fractal.bin -fopenmp
@@ -10,8 +12,8 @@ gfortran fractal.f90 -o fractal.bin -fopenmp
 ### With Intel Fortran
 ifort fractal.f90 -o fractal.bin -qopenmp
 
-### Run binary
-fractal.bin > filename.dat
+### Run Binary
+./fractal.bin #This will create "data.dat" fortran array binary file. 
 
 ### Visualize Image
 Use Visualize.ipynb notebook.
--- fractal.f90
+++ fractal.f90
@@ -1,10 +1,10 @@
 PROGRAM FRACTAL
         IMPLICIT NONE
-        INTEGER X,Y,I,ITER_MAX,XRES,YRES,THREAD,CHUNKSIZE
+        INTEGER X,Y,I,ITER_MAX,XRES,YRES,MARKER
         REAL XMIN,XMAX,YMIN,YMAX,THRESHOLD
 ! X Part=Real, Y Part=Imagenary, I Max iter. 
         COMPLEX ZN,C
-        PARAMETER(ITER_MAX=2550,XMIN=-2,XMAX=-1.5,YMIN=-0.2,YMAX=0.2,XRES=50000,YRES=40000,THRESHOLD=1024)
+        PARAMETER(ITER_MAX=255,XMIN=-2,XMAX=1,YMIN=-1,YMAX=1,XRES=15000,YRES=10000,THRESHOLD=1024)
         INTEGER Z_ARRAY(XRES,YRES)
 
         Z_ARRAY=ITER_MAX ! Initialize Z_array as max iter. 
@@ -16,10 +16,8 @@ PROGRAM FRACTAL
                         ZN=CMPLX(0,0)
                         C=CMPLX(XMIN+(XMAX-XMIN)*X/XRES,YMIN+(YMAX-YMIN)*Y/YRES)
                         DO I=0,ITER_MAX
-                                !ZN=(ZN+C)**2
-                                !ZN=(ZN+C)**(5)
-                                !ZN=ZN**2+0.19*ZN**3+C
-                                ZN=(ABS(REAL(ZN))+CMPLX(0,1)*ABS(AIMAG(ZN)))**2+C
+                                ZN=(ZN+C)**2 !Mandelbrot
+                                !ZN=(ABS(REAL(ZN))+CMPLX(0,1)*ABS(AIMAG(ZN)))**2+C !Burning ship
                                 IF (ABS(ZN)>THRESHOLD) THEN
                                         Z_ARRAY(X,Y)=I
                                         GOTO 110
@@ -30,7 +28,8 @@ PROGRAM FRACTAL
         END DO
         !$OMP END DO NOWAIT
         !$OMP END PARALLEL
-
-        
-        PRINT*,Z_ARRAY
+        INQUIRE(IOLENGTH=MARKER) Z_ARRAY
+        OPEN(10,file="data.dat",form="unformatted",access='stream')
+        WRITE(10)MARKER,Z_ARRAY,MARKER
+        CLOSE(10)
         END PROGRAM FRACTAL
