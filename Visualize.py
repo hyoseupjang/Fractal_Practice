4a7221f25b705364aafe1b0ce796672a5daa7b21
Hyoseup Jang <hyoseupjang@gmail.com>
2020-11-07 12:54:33 +0900

    asdf

--- Visualize.py
+++ Visualize.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
-data=np.fromfile('data.dat',dtype=np.int32)
+xres,yres=np.fromfile('data.dat',dtype=np.int32,count=2,offset=0)
+data=np.fromfile('data.dat',dtype=np.int32,offset=8)
 print('File loaded...')
-data=np.array(data.reshape(80000,120000),dtype=np.uint8)
+data=np.array(data.reshape(yres,xres),dtype=np.uint8)
 print('Converted to uint8... Rendering. ')
 Image.fromarray(data,'L').save('output.png')
